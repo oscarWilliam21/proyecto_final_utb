73db8da9ea3fa63d8ec67219238674901d4738a6
oscarWilliam21 <oscarwilliam210890@gmail.com>
2025-05-31 11:36:56 -0500

    solucion de duplicate de user register

--- api/routes/user_route.py
+++ api/routes/user_route.py
@@ -4,6 +4,6 @@ from controllers.user_controller import register_user
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route("/register", methods=["POST"])
-def register_user():
+def register_user_route():
     data = request.get_json()
     return register_user(data)
