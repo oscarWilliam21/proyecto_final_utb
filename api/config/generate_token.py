59746e8002cd0eafc2c522633a1bbc1be0e60eca
luisSLAyer <161789755+luisSLAyer@users.noreply.github.com>
2025-06-06 16:17:34 -0500

    ok

--- api/config/generate_token.py
+++ api/config/generate_token.py
@@ -2,22 +2,24 @@ import os
 import jwt
 import datetime
 from dotenv import load_dotenv
-
+ 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 REFRESH_SECRET = os.getenv("REFRESH_SECRET")
 
+print(SECRET_KEY, REFRESH_SECRET)
+ 
 def create_access_token(usuario_id):
     payload = {
         "user_id": str(usuario_id),
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1) 
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
     }
-    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    token = jwt.encode(payload, str(SECRET_KEY), algorithm="HS256")
     return token
-
+ 
 def create_refresh_token(user_id):
     payload = {
         "user_id": str(user_id),
         "exp": datetime.datetime.utcnow() + datetime.timedelta(days=7)
     }
-    return jwt.encode(payload, REFRESH_SECRET, algorithm="HS256")
+    return jwt.encode(payload, str(REFRESH_SECRET), algorithm="HS256")
